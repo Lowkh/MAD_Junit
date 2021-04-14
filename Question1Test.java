dc84b627e9b618cc84b43aba5937c3a60b667a48
Lowkh <54532616+Lowkh@users.noreply.github.com>
2021-04-14 23:24:31 +0800

    Update Question1Test.java

--- Question1Test.java
+++ Question1Test.java
@@ -1,9 +1,30 @@
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 
 public class Question1Test {
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+
+    
+    @Before
+    public void setStreams() {
+        System.setOut(new PrintStream(out));
+  
+    }
     @Test
     public void testPrint(){
-        Question1 q = new Question1();
-        
-        assertEquals("Hello World!",q);
+        Question1.main(new String[]{"a"});
+        Assert.assertEquals("Hello World!", out.toString().trim());
+    }
+    
+    @After
+    public void restoreInitialStreams() {
+        System.setOut(originalOut);
     }
 }
