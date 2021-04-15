250f9e7d0dcd1c5512d8fefedf607e128180e4e0
Lowkh <54532616+Lowkh@users.noreply.github.com>
2021-04-15 18:53:22 +0800

    Create Question3Test.java

---
+++ Question3Test.java
@@ -0,0 +1,40 @@
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class Question3Test {
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+  
+    
+    @Before
+    public void setStreams() {
+        System.setOut(new PrintStream(out));
+  
+    }
+  
+    @Test
+    public void BMITest() {
+      String testVal = "10\n";
+      byte[] testVal_bytes = testVal.getBytes();
+      provideInput(testVal_bytes);
+      Question3.main(new String[]{"a"});
+      Assert.assertEquals("100", out.toString().trim());
+    }
+  
+    private void provideInput(byte[] int_bytes){
+      ByteArrayInputStream testIn = new ByteArrayInputStream(int_bytes);
+      System.setIn(testIn);
+    }
+  
+    @After
+    public void restoreInitialStreams() {
+        System.setOut(originalOut);
+    }
+}
