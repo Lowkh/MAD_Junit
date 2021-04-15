9ddf89bb512c33ec6d83ac8c823367f265cfeb6b
Lowkh <54532616+Lowkh@users.noreply.github.com>
2021-04-15 12:49:04 +0800

    Create Question2Test.java

---
+++ Question2Test.java
@@ -0,0 +1,30 @@
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class Question1Test {
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+
+    
+    @Before
+    public void setStreams() {
+        System.setOut(new PrintStream(out));
+  
+    }
+    @Test
+    public void testPrint(){
+        Question2.main(new String[]{"a"});
+        Assert.assertEquals("0.1", out.toString().trim());
+    }
+    
+    @After
+    public void restoreInitialStreams() {
+        System.setOut(originalOut);
+    }
+}
